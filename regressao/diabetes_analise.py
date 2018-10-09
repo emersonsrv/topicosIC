bc59b1658d0a1aa3b5e77e0f9c95ad858c208a0b
emersonsrv <emersonsrv16@gmail.com>
2018-10-09 09:25:56 -0300

    atualiza codigo

--- regressao/diabetes_analise.py
+++ regressao/diabetes_analise.py
@@ -3,30 +3,30 @@ import pandas
 import matplotlib.pyplot as plt
 
 target = 'dp'
-
+# carrega a tabela de dados
 diabetes = load_diabetes()
 tabela = pandas.DataFrame(diabetes.data)
 tabela.columns = diabetes.feature_names
 tabela[target] = diabetes.target
-
+# gera o grafico(.png) de correlacao entre os atributos IMC e DP
 plt.scatter(tabela.bmi, tabela.dp)
 plt.xlabel('IMC')
 plt.ylabel('Dissease Prog.')
 plt.savefig('correlacao-1.png')
 plt.close()
-
+# gera o grafico(.png) de correlacao entre os atributos S3 e DP
 plt.scatter(tabela.s3, tabela.dp)
 plt.xlabel('S3')
 plt.ylabel('Dissease Prog.')
 plt.savefig('correlacao-2.png')
 plt.close()
-
+# gera o grafico(.png) de correlacao entre os atributos S5 e DP
 plt.scatter(tabela.s5, tabela.dp)
 plt.xlabel('S5')
 plt.ylabel('Dissease Prog.')
 plt.savefig('correlacao-3.png')
 
-
+# grava arquivo com database headset
 print(tabela.head(20))
 f = open('diabetes20.txt', 'w')
 f.write(str(tabela.head(20)))
--- regressao/diabetes_corr.py
+++ regressao/diabetes_corr.py
@@ -3,12 +3,12 @@ import pandas
 import matplotlib.pyplot as plt
 
 target = 'dp'
-
+# gera tabela de correlacao
 diabetes = load_diabetes()
 tabela = pandas.DataFrame(diabetes.data)
 tabela.columns = diabetes.feature_names
 tabela[target] = diabetes.target
-
+# grava arquivo com a tabela de correlacao
 print(tabela.corr())
 f = open('diabetes_corr.txt', 'w')
 f.write(str(tabela.corr()))

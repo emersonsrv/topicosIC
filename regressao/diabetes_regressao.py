9072e3369f935f2749984de4297d1f7273380d59
emersonsrv <emersonsrv16@gmail.com>
2018-10-09 09:36:30 -0300

    atualiza codigo

--- regressao/diabetes_regressao.py
+++ regressao/diabetes_regressao.py
@@ -5,9 +5,9 @@ import matplotlib.pyplot as plt
 
 target = 'dp'
 
-#Atributos escolhidos
+# Atributos escolhidos
 
-#attribute1 = 'bmi'
+# attribute1 = 'bmi'
 attribute1 = 's5'
 attribute2 = 's3'
 
@@ -17,10 +17,10 @@ tabela.columns = diabetes.feature_names
 tabela[target] = diabetes.target
 
 
-#seleciona duas colunas
+# seleciona duas colunas
 X = tabela[[attribute1, attribute2]]
 
-#separa em dois conjuntos, um para treinamento e outro para validação (20 últimos)
+# separa em dois conjuntos, um para treinamento e outro para validação (20 últimos)
 X_t = X[:-20]
 X_v = X[-20:]
 y_t = tabela[target][:-20]
@@ -37,21 +37,21 @@ y_pred = regr.predict(X_v)
 # coeficientes a
 a = regr.coef_
 print('Coeficientes: \n', a)
-#intercepto b
+# intercepto b
 b = regr.intercept_
 print('Coeficientes: \n', b)
-#y = 5.10*RM + -0.65*LSTAT + -1.24
+# y = 5.10*RM + -0.65*LSTAT + -1.24
 
-#prediz manualmente os valores com base nos coeficientes encontrados na regressao
+# prediz manualmente os valores com base nos coeficientes encontrados na regressao
 y_teste = a[0]*X_v[attribute1] + a[1]*X_v[attribute2] + b
 
-#exibe o valor predito manualmente y_teste, que começa de 486
-#exibe o valor real y_t
-#exibe o valor predito pela regressão linear
+# exibe o valor predito manualmente y_teste, que começa de 486
+# exibe o valor real y_t
+# exibe o valor predito pela regressão linear
 
 print(y_teste[len(X)-20], y_t[0],y_pred[0])
 
-#plota todos os valores de validação
+# plota todos os valores de validação
 plt.scatter(X_v[attribute1], y_v,  color='black')
 plt.scatter(X_v[attribute1], y_pred, color='blue')
 plt.legend(["Real", "Predito"])

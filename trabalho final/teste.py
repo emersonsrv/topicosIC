ca126dbeeb4f1d53dfca5f2e4c0709bc29a66ec4
cantuariac <caio.cantuaria@gmail.com>
2019-02-02 15:23:30 -0200

    base de dados na raiz do repositorio

--- trabalho final/teste.py
+++ trabalho final/teste.py
@@ -12,7 +12,7 @@ from sklearn.preprocessing import StandardScaler
 edges = [[20, 1, 2, 1,  8, 10, 2,  9, 11, 3, 4, 7, 7,  5, 6,  14, 15, 16, 17],
 		[  3, 3, 3, 8, 10, 12, 9, 11, 13, 4, 7, 5, 6, 14, 15, 16, 17, 18, 19]]
 
-basePath = '../../MSRAction3D/skeleton/'
+basePath = '../MSRAction3D/skeleton/'
 
 ## Carrega instancia do arquivo
 def loadData(action, subject, instance):
--- trabalho intermediario/teste.py
+++ trabalho intermediario/teste.py
@@ -7,7 +7,7 @@ import time
 edges = [[20, 1, 2, 1,  8, 10, 2,  9, 11, 3, 4, 7, 7,  5, 6,  14, 15, 16, 17],
 		[  3, 3, 3, 8, 10, 12, 9, 11, 13, 4, 7, 5, 6, 14, 15, 16, 17, 18, 19]]
 
-basePath = '../../MSRAction3D/skeleton/'
+basePath = '../MSRAction3D/skeleton/'
 
 def loadData(action, subject, instance):
 	ins = np.loadtxt(basePath + 'a%02i_s%02i_e%02i_skeleton.txt'%(action, subject, instance))
